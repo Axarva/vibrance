421102478c572e7d8751972eaa06941055b6ec8a
Axarva <66304313+Axarva@users.noreply.github.com>
2020-11-05 10:51:27 +0545

    Update autostart.sh

--- autostarts/autostart.sh
+++ autostarts/autostart.sh
@@ -1,9 +1,9 @@
-exec /home/axarva/.local/bin/wal -R &
-exec feh --bg-scale /home/axarva/wallpapers/faceless.png &
-/usr/local/bin/picom -f &
+exec /home/$USER/.local/bin/wal -R &
+exec feh --bg-scale /home/$USER/wallpapers/faceless.png & #Point this to your wallpaper
+/usr/local/bin/picom -f & #remove /usr/local/bin if not using a picom fork
 flameshot &
 dunst &
-exec /home/axarva/dwm/dwm-bar/dwm_bar.sh &
-exec /home/axarva/bin/lock.sh &
+exec /home/$USER/dwm/dwm-bar/dwm_bar.sh & 
+exec /home/$USER/bin/lock.sh &
 #exec /usr/libexec/polkit-gnome-authentication-agent-1 &
 exec greenclip daemon
